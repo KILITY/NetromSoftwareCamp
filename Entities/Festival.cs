c64efbbde0415f45c633c13e0167484dc64e9c21
c.andrei25.AC@gmail.com <c.andrei25.AC@gmail.com>
2025-07-01 19:20:31 +0300

    Complete CRUD Operations for Bands And Festivals, plus Management Page

--- Entities/Festival.cs
+++ Entities/Festival.cs
@@ -6,8 +6,8 @@ public class Festival
     public string Name { get; set; }
     public string Description { get; set; }
     public string Location { get; set; }
-    public DateTime StartDate { get; set; } 
-    public DateTime EndDate { get; set; }
+    public DateTime? StartDate { get; set; } 
+    public DateTime? EndDate { get; set; }
     
     public ICollection<Band> Bands { get; set; }
     public ICollection<Booking> Bookings { get; set; }
