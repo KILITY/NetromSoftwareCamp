67d79b4365b9a6cfd86cc90861f130bb9bbee346
c.andrei25.AC@gmail.com <c.andrei25.AC@gmail.com>
2025-07-09 23:19:37 +0300

    Made some changes to ui and migration for FestivalPrice

--- Entities/Festival.cs
+++ Entities/Festival.cs
@@ -6,6 +6,8 @@ public class Festival
     public string Name { get; set; }
     public string Description { get; set; }
     public string Location { get; set; }
+    
+    public int Price { get; set; }
     public DateTime? StartDate { get; set; } 
     public DateTime? EndDate { get; set; }
     
