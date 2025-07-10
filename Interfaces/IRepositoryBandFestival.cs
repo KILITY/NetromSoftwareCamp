0d3216e991cbc94fc0edd20a68e76413f4d1ac95
c.andrei25.AC@gmail.com <c.andrei25.AC@gmail.com>
2025-07-10 15:55:02 +0300

    added band booked logic

--- Interfaces/IRepositoryBandFestival.cs
+++ Interfaces/IRepositoryBandFestival.cs
@@ -5,4 +5,6 @@ namespace MyApplication.Interfaces;
 public interface IRepositoryBandFestival : IRepositoryBase<BandFestival>
 {
     Task<List<Band>> GetBandsByFestivalIdAsync(Guid festivalId);
+
+    Task<List<Guid>> GetBandIdsInDateRangeAsync(DateTime? start, DateTime? end, Guid? excludeFestivalId = null);
 }
--- Repositories/RepositoryBandFestival.cs
+++ Repositories/RepositoryBandFestival.cs
@@ -20,4 +20,18 @@ public class RepositoryBandFestival : RepositoryBase<BandFestival>, IRepositoryB
             .Select(bf => bf.Band)
             .ToListAsync();
     }
+    
+    public async Task<List<Guid>> GetBandIdsInDateRangeAsync(DateTime? start, DateTime? end, Guid? excludeFestivalId = null)
+    {
+        var query = Context.BandFestivals
+            .Include(bf => bf.Festival)
+            .Where(bf =>
+                EF.Functions.DateDiffDay(bf.Festival.StartDate, end.Value) <= 0 &&
+                EF.Functions.DateDiffDay(bf.Festival.EndDate, start.Value) >= 0);
+
+        if (excludeFestivalId.HasValue)
+            query = query.Where(bf => bf.FestivalId != excludeFestivalId.Value);
+
+        return await query.Select(bf => bf.BandId).Distinct().ToListAsync();
+    }
 }
